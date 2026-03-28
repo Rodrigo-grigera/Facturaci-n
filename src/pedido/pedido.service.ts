f3d1553147d6f550547dc52ecb4247795e6dcf43
Rodrigo Grigera <rodrigogrigera@gmail.com>
2026-03-28 19:58:50 -0300

    creacion de logica en service pedido

--- src/pedido/pedido.service.ts
+++ src/pedido/pedido.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { responseDTO } from 'src/producto/dto/responseDTO';
+import { Repository } from 'typeorm';
+import { Pedido } from './entities/pedido.entity';
 
 @Injectable()
 export class PedidoService {
-  create(createPedidoDto: CreatePedidoDto) {
-    return 'This action adds a new pedido';
+  constructor(@InjectRepository(Pedido) private readonly pedidoRepository : Repository <Pedido>){}
+
+  async create(createPedido: CreatePedidoDto) : Promise <responseDTO> {
+    const newPedido = this.pedidoRepository.create(createPedido);
+    const resp = await this.pedidoRepository.save(newPedido);
+    if(!resp) throw new NotFoundException ('Pedido no creado')
+    return {
+          message : 'Pedido creado',
+          code : HttpStatus.CREATED,
+          data : resp
+    };
   }
 
-  findAll() {
-    return `This action returns all pedido`;
+  async findAll() : Promise <responseDTO> {
+    const pedidos = await this.pedidoRepository.find()
+    if(!pedidos) throw new NotFoundException('No se encontraron pedidos');
+    return {
+        message : 'Todos los pedidos',
+        code : HttpStatus.OK,
+        data : pedidos
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pedido`;
+  async findOne(id: number) : Promise <responseDTO> {
+    const resp = await this.pedidoRepository.findOneBy({id_pedido : id});
+    if(!resp) throw new NotFoundException ('No se encontro pedido')
+    return{
+        message : 'Pedido encontrado',
+        code : HttpStatus.OK,
+        data : resp 
+    } ;
   }
 
-  update(id: number, updatePedidoDto: UpdatePedidoDto) {
-    return `This action updates a #${id} pedido`;
+  async update(id: number, updatePedidoDto: UpdatePedidoDto) : Promise <responseDTO> {
+    const updatePedido = await this.pedidoRepository.update({id_pedido : id}, updatePedidoDto);
+    if(!updatePedido.affected) throw new NotFoundException('Error al modificar el pedido');
+    return {
+          message : 'Pedido modificado',
+          code : HttpStatus.NO_CONTENT
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pedido`;
+  async remove(id: number) : Promise <responseDTO> {
+    const deletePedido = await this.pedidoRepository.delete({id_pedido : id});
+    if(!deletePedido.affected) throw new NotFoundException('Error al eliminar pedido')
+    return {
+          message : 'Pedido eliminado',
+          code : HttpStatus.NO_CONTENT
+    };
   }
 }
